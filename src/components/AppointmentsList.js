195c27e2bfa133711032e4e9e8581bac7ab2e2dd
Dante <>
2019-08-23 16:01:51 -0500

    ADD conditional text on AppointmentList

--- src/components/AppointmentsList.js
+++ src/components/AppointmentsList.js
@@ -3,23 +3,30 @@ import React from 'react'
 import Appointment from './Appointment'
 import PropTypes from 'prop-types'
 
-const AppointmentsList = ({ appointments, eraseAppointment }) => (
-  <div className='card mt-2 py-5'>
-    <div className='card-body'>
-      <h2 className='card-title text-center'>Admin your Appointments here!</h2>
+const AppointmentsList = ({ appointments, eraseAppointment }) => {
+  const message =
+    Object.keys(appointments).length === 0
+      ? 'No Appointments'
+      : 'Admin your Appointments here!'
 
-      <div className='lista-citas'>
-        {appointments.map(appointment => (
-          <Appointment
-            key={appointment.id}
-            appointment={appointment}
-            eraseAppointment={eraseAppointment}
-          />
-        ))}
+  return (
+    <div className='card mt-2 py-5'>
+      <div className='card-body'>
+        <h2 className='card-title text-center'>{message}</h2>
+
+        <div className='lista-citas'>
+          {appointments.map(appointment => (
+            <Appointment
+              key={appointment.id}
+              appointment={appointment}
+              eraseAppointment={eraseAppointment}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 AppointmentsList.propTypes = {
   appointments: PropTypes.array.isRequired,
